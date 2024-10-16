b43c0f60a9b69ff8df93ca5a0cbcc977db6b56e7
Harry Shih <cool9850311cool@gmail.com>
2024-10-16 13:55:05 +0800

    refactor: reconstruct router

--- Go-Service/src/main/infrastructure/router/router.go
+++ Go-Service/src/main/infrastructure/router/router.go
@@ -19,27 +19,22 @@ import (
 )
 
 func NewRouter(db *mongo.Database, log logger.Logger, liveStreamService stream.ILivestreamService, redisClient *redis.Client) *gin.Engine {
-	var r *gin.Engine
-	r = gin.Default()
-	if !config.AppConfig.Server.EnableGinLog {
-		r = gin.New()
-		r.Use(gin.Recovery())
-	}
-	systemSettingRepo := repository.NewMongoSystemSettingRepository(db)
-	systemSettingUseCase := usecase.NewSystemSettingUseCase(systemSettingRepo, log)
-	systemSettingController := controller.NewSystemSettingController(log, systemSettingUseCase)
-	discordOAuthOuterApi := discord.NewDiscordOAuthImpl()
-	jwtGenerator := util.NewJWTLibrary()
-	discordLoginUseCase := usecase.NewDiscordLoginUseCase(systemSettingRepo, log, config.AppConfig, discordOAuthOuterApi, jwtGenerator)
-	
-	discordOauthController := controller.NewDiscordOauthController(log, discordLoginUseCase)
-	livestreamRepo := repository.NewMongoLivestreamRepository(db)
-	viewerCountCache := cache.NewRedisViewerCount(redisClient)
-	chatCache := cache.NewRedisChat(redisClient)
-	livestreamUseCase := usecase.NewLivestreamUsecase(livestreamRepo, log, config.AppConfig, liveStreamService, viewerCountCache, chatCache)
-	livestreamController := controller.NewLivestreamController(log, livestreamUseCase)
+	r := setupRouter()
+	setupMiddlewares(r)
+	setupRoutes(r, db, log, liveStreamService, redisClient)
+	return r
+}
 
-	// Add CORS middleware to allow all origins
+func setupRouter() *gin.Engine {
+	if !config.AppConfig.Server.EnableGinLog {
+		r := gin.New()
+		r.Use(gin.Recovery())
+		return r
+	}
+	return gin.Default()
+}
+
+func setupMiddlewares(r *gin.Engine) {
 	r.Use(cors.New(cors.Config{
 		AllowAllOrigins:  true,
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
@@ -47,7 +42,6 @@ func NewRouter(db *mongo.Database, log logger.Logger, liveStreamService stream.I
 		AllowCredentials: true,
 	}))
 
-	// Handle OPTIONS requests globally
 	r.Use(func(c *gin.Context) {
 		if c.Request.Method == "OPTIONS" {
 			c.Header("Access-Control-Allow-Origin", "*")
@@ -61,20 +55,53 @@ func NewRouter(db *mongo.Database, log logger.Logger, liveStreamService stream.I
 	})
 
 	r.Use(middleware.TraceIDMiddleware())
-	r.GET("/oauth/discord", discordOauthController.Callback)
-	r.GET("/livestream/:uuid/:filename", middleware.JWTAuthMiddleware(log), livestreamController.GetFile)
-	r.GET("/system-settings", middleware.JWTAuthMiddleware(log), systemSettingController.GetSetting)
-	r.PATCH("/system-settings", middleware.JWTAuthMiddleware(log), systemSettingController.SetSetting)
-	r.POST("/livestream", middleware.JWTAuthMiddleware(log), livestreamController.CreateLivestream)
-	r.GET("/livestream/owner/:user_id", middleware.JWTAuthMiddleware(log), livestreamController.GetLivestreamByOwnerId)
-	r.GET("/livestream/one", middleware.JWTAuthMiddleware(log), livestreamController.GetLivestreamOne)
-	r.PATCH("/livestream/:uuid", middleware.JWTAuthMiddleware(log), livestreamController.UpdateLivestream)
-	r.DELETE("/livestream/:uuid", middleware.JWTAuthMiddleware(log), livestreamController.DeleteLivestream)
-	r.GET("/livestream/ping-viewer-count/:uuid", middleware.JWTAuthMiddleware(log), livestreamController.PingViewerCount)
-	r.GET("/livestream/chat/:uuid/:index", middleware.JWTAuthMiddleware(log), livestreamController.GetChat)
-	r.GET("/livestream/chat/delete/:uuid", middleware.JWTAuthMiddleware(log), livestreamController.GetDeleteChatIDs)
-	r.POST("/livestream/chat", middleware.JWTAuthMiddleware(log), livestreamController.AddChat)
-	r.DELETE("/livestream/chat/:uuid/:chat_id", middleware.JWTAuthMiddleware(log), livestreamController.RemoveViewerCount)
-	r.POST("/livestream/mute-user", middleware.JWTAuthMiddleware(log), livestreamController.MuteUser)
-	return r
+}
+
+func setupRoutes(r *gin.Engine, db *mongo.Database, log logger.Logger, liveStreamService stream.ILivestreamService, redisClient *redis.Client) {
+	// Initialize repositories, use cases, and controllers
+	systemSettingRepo := repository.NewMongoSystemSettingRepository(db)
+	systemSettingUseCase := usecase.NewSystemSettingUseCase(systemSettingRepo, log)
+	systemSettingController := controller.NewSystemSettingController(log, systemSettingUseCase)
+	discordOAuthOuterApi := discord.NewDiscordOAuthImpl()
+	jwtGenerator := util.NewJWTLibrary()
+	discordLoginUseCase := usecase.NewDiscordLoginUseCase(systemSettingRepo, log, config.AppConfig, discordOAuthOuterApi, jwtGenerator)
+	discordOauthController := controller.NewDiscordOauthController(log, discordLoginUseCase)
+	livestreamRepo := repository.NewMongoLivestreamRepository(db)
+	viewerCountCache := cache.NewRedisViewerCount(redisClient)
+	chatCache := cache.NewRedisChat(redisClient)
+	livestreamUseCase := usecase.NewLivestreamUsecase(livestreamRepo, log, config.AppConfig, liveStreamService, viewerCountCache, chatCache)
+	livestreamController := controller.NewLivestreamController(log, livestreamUseCase)
+
+	login := r.Group("/")
+	{
+		login.GET("/oauth/discord", discordOauthController.Callback)
+	}
+
+	systemSettings := r.Group("/system-settings")
+	{
+		systemSettings.GET("", middleware.JWTAuthMiddleware(log), systemSettingController.GetSetting)
+		systemSettings.PATCH("", middleware.JWTAuthMiddleware(log), systemSettingController.SetSetting)
+	}
+
+	livestream := r.Group("/livestream")
+	{
+		livestream.GET("/:uuid/:filename", middleware.JWTAuthMiddleware(log), livestreamController.GetFile)
+		livestream.POST("", middleware.JWTAuthMiddleware(log), livestreamController.CreateLivestream)
+		livestream.GET("/owner/:user_id", middleware.JWTAuthMiddleware(log), livestreamController.GetLivestreamByOwnerId)
+		livestream.GET("/one", middleware.JWTAuthMiddleware(log), livestreamController.GetLivestreamOne)
+		livestream.PATCH("/:uuid", middleware.JWTAuthMiddleware(log), livestreamController.UpdateLivestream)
+		livestream.DELETE("/:uuid", middleware.JWTAuthMiddleware(log), livestreamController.DeleteLivestream)
+		livestream.GET("/ping-viewer-count/:uuid", middleware.JWTAuthMiddleware(log), livestreamController.PingViewerCount)
+
+		chat := livestream.Group("/chat")
+		{
+			chat.GET("/:uuid/:index", middleware.JWTAuthMiddleware(log), livestreamController.GetChat)
+			// get deleted chat message
+			chat.GET("/delete/:uuid", middleware.JWTAuthMiddleware(log), livestreamController.GetDeleteChatIDs)
+			chat.POST("", middleware.JWTAuthMiddleware(log), livestreamController.AddChat)
+			chat.DELETE("/:uuid/:chat_id", middleware.JWTAuthMiddleware(log), livestreamController.RemoveViewerCount)
+		}
+
+		livestream.POST("/mute-user", middleware.JWTAuthMiddleware(log), livestreamController.MuteUser)
+	}
 }
